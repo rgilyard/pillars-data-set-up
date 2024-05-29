b34b79e4cff9aee2a36d529fef8ea27eb4515d83
rgilyard <gilyardrachel@gmail.com>
2024-05-28 17:57:31 -0700

    Add function to zip prepared data directory

--- python_scripts/set_up.py
+++ python_scripts/set_up.py
@@ -1,4 +1,6 @@
 from datasets import *
+import zipfile
+import pathlib
 
 
 def prepare_data():
@@ -7,7 +9,24 @@ def prepare_data():
     # Will I even need to do anything with ARCS?
 
 
+def zip_data():
+    # output_file = 'arcs.zip'
+    # input_dir = 'arcs'
+    # with zipfile.ZipFile(output_file, 'w') as myzip:
+    #     myzip.write(input_dir)
+
+    directory = pathlib.Path('arcs/')
+
+    with zipfile.ZipFile('arcs.zip', mode='w') as archive:
+        for file_path in directory.rglob('*'):
+            print(file_path)
+            archive.write(file_path, arcname=file_path.relative_to(directory))
+
+
 if __name__ == '__main__':
     print('attempting to prepare data')
     prepare_data()
     print('done preparing data')
+    print('zipping data')
+    zip_data()
+    print('done zipping data')
